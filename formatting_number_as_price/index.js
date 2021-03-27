3672f162d8b8da4cdb55e3b23f4c423316ccd097
nicole-gathany <nicole.gathany@gmail.com>
2021-03-26 22:53:53 -0400

    solution passed 11 out of 12 tests. i will just look at other solutions at this point

--- formatting_number_as_price/index.js
+++ formatting_number_as_price/index.js
@@ -1,29 +1,36 @@
-var numberToPrice = function(number) {
-    //first it has has to be a number?
-     let start = number.toString().split(".");
-     console.log(start)
-     if(number>=0||number<=0){
-       //i have to account for negative numbers too which is annoying!!!
-        let arr = start[0].split("")
-      console.log(arr)
-         if(arr.length%3>0){
-           for(i=arr.length-3; i>0; i-=3){
-             arr.splice(i,0, ",")
-           }
-           console.log(arr.join(""))
-         }
-       
-       
-     
-     if(start.length === 2){
-      console.log(start[0].length)
-       console.log(start[0].length%3)
-     
-       
-     }
-      else if(start.length==1){
-       return start[0] + ".00"
-     }
-       }
-     else  return "" + Number(number)
-   }
+var numberToPrice = function (number) {
+  //first it has has to be a number?
+  //this is passing 11 out of the 12 tests. I'm not sure what is going wrong
+  console.log(number);
+  let start = number.toString().split(".");
+  console.log(start);
+  if (number >= 0 || number <= 0) {
+    let arr = start[0].split("");
+
+    if (number > -99)
+      for (i = arr.length - 3; i > 0; i -= 3) {
+        arr.splice(i, 0, ",");
+      }
+    start[0] = arr.join("");
+    console.log(start[0]);
+
+    if (number < -99) {
+      for (i = arr.length - 3; i > 1; i -= 3) {
+        arr.splice(i, 0, ",");
+      }
+      start[0] = arr.join("");
+    }
+
+    if (start.length === 2) {
+      if (start[1].length >= 2) {
+        return start[0] + "." + start[1].substring(0, 2);
+      } else if (start[1].length == 1) {
+        return start[0] + "." + start[1] + "0";
+      }
+    } else if (start.length == 1) {
+      return start[0] + ".00";
+    }
+  } else return "" + Number(number);
+};
+
+console.log(numberToPrice(-999));
