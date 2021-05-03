421a2ef0d9986e4b771072081f76422c723162c3
nicole-gathany <nicole.gathany@gmail.com>
2021-05-02 23:39:25 -0400

    added best practice

--- array_problems/SmallEnough/Best_Practice/index.js
+++ array_problems/SmallEnough/Best_Practice/index.js
@@ -1,3 +1,6 @@
 function smallEnough(a, limit){
-    return a.every(item => item <=limit);
+    return Math.max(...a) <= limit
   }
+
+  // i wonder if if this might be a better solution because it's only comparing the
+  // highest number to the limit, instead of every value.
