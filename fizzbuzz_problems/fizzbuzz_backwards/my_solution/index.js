5ab966c5e4279f1a7fbb98c484af691d50783be2
nicole-gathany <nicole.gathany@gmail.com>
2021-05-04 23:15:47 -0400

    fizz buzz reverse, my solution and best practices

---
+++ fizzbuzz_problems/fizzbuzz_backwards/my_solution/index.js
@@ -0,0 +1,28 @@
+function reverseFizzBuzz(array) {
+    // your code
+    //parameter is an array
+    console.log(array)
+    let fizz = []; 
+    let buzz = [];
+    let fizzbuzz = [];
+    let result = []
+    for(let i=0; i<array.length; i++){
+      if(array[i]==="Fizz"){fizz.push(i+1)}
+      else if(array[i]==="Buzz"){buzz.push(i+1)}
+      else if(array[i]==="FizzBuzz"){fizzbuzz.push(i+1)}
+    }
+    console.log("fizz is " + fizz);
+    console.log("buzz is " + buzz);
+    console.log("fizzbuzz is" + fizzbuzz);
+    if(fizzbuzz.length === 0 ){ result.push(fizz[0], buzz[0])}
+    else if(fizz.length!==0 && fizzbuzz.length!==0){
+      result.push(fizz[0], fizzbuzz[0]/fizz[0])
+    } else if(fizz.length===0&&buzz.length===0&&fizzbuzz.length!==0){
+      result.push(fizzbuzz[0], fizzbuzz[0])
+    } else if(fizz.length===0&&buzz.length!==0&&fizzbuzz.length!==0){
+      result.push( fizzbuzz[0], buzz[0])
+    }
+    
+  
+    return result;
+  };
