1e76ec982435155b7cde341d5cc7a28d0b3b31b0
nicole-gathany <nicole.gathany@gmail.com>
2021-05-12 23:43:55 -0400

    more notes

--- objects/Lets_recycle/Best_practice/index.js
+++ objects/Lets_recycle/Best_practice/index.js
@@ -3,6 +3,7 @@
 const recycle = array =>
   [ 'paper', 'glass', 'organic', 'plastic' ]
     .map(mat => array.filter(obj => obj.material === mat || obj.secondMaterial === mat)
-    .map(obj => obj.type))
+    .map(obj => obj.type ))
 
 //the solution is elegant. however, i would not use this solution because it maps the array twice and filters it in the same line, that sounds like it would loop 3xs which means that the time complexity would be O(n*n*n)
+//i'm going to count the number of calls using console.count()
