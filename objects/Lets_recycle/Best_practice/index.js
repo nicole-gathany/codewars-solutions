3f6f46a02c55686c23702555e782b78c3f1f246c
nicole-gathany <nicole.gathany@gmail.com>
2021-05-12 23:40:42 -0400

    best practice solution which may not be the best practice

---
+++ objects/Lets_recycle/Best_practice/index.js
@@ -0,0 +1,8 @@
+//my solution is considered one of the best practices (it got 8 votes for best practice)
+//however, i wanted to include the solutions using higher order functions
+const recycle = array =>
+  [ 'paper', 'glass', 'organic', 'plastic' ]
+    .map(mat => array.filter(obj => obj.material === mat || obj.secondMaterial === mat)
+    .map(obj => obj.type))
+
+//the solution is elegant. however, i would not use this solution because it maps the array twice and filters it in the same line, that sounds like it would loop 3xs which means that the time complexity would be O(n*n*n)
