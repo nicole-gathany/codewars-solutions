3518dcd013007ecd4fc26b5e866462874ce8cfd6
nicole-gathany <nicole.gathany@gmail.com>
2021-05-28 23:12:48 -0400

    new strings problem

---
+++ strings/vowel_count/index.js
@@ -0,0 +1,6 @@
+function getCount(str) {
+    return str.split("").filter(letter => "aeiou".includes(letter)).length;
+  }
+
+  //to be honest, they didn't save my solution so I lost it :(
+  //but i looked at the best practice solutions and was inspired
