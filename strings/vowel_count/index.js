4c2ef3696d624c8fc9b8ebe8001d61342c26d107
nicole-gathany <nicole.gathany@gmail.com>
2021-05-28 23:22:15 -0400

    notes

--- strings/vowel_count/index.js
+++ strings/vowel_count/index.js
@@ -4,3 +4,14 @@ function getCount(str) {
 
   //to be honest, they didn't save my solution so I lost it :(
   //but i looked at the best practice solutions and was inspired
+
+  //my original was more like:
+//   function getCount(str) {
+//     var vowelsCount = 0;
+//     str.split("").forEach(function(x){
+//       if(x == "a" | x == "e" | x == "i" | x == "o" | x == "u"){
+//         vowelsCount += 1;
+//       }
+//     });  
+//     return vowelsCount;
+//   }
