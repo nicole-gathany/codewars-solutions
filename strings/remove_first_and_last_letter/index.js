2c7d7bdfd6529f8de8380144c0e2ac81dc96561a
nicole-gathany <nicole.gathany@gmail.com>
2021-06-07 22:50:32 -0400

    8kyu string problem

---
+++ strings/remove_first_and_last_letter/index.js
@@ -0,0 +1,11 @@
+function removeChar(str){
+    //You got this!
+    //parameter is a string
+     //return a new string with the beginning and end missing
+     return str.substring(1, str.length-1);
+   };
+
+//    another option:
+//    function removeChar(str) {
+//     return str.slice(1, -1);
+//   }
