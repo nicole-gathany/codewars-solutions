bdb23f1521bcee474f60c735354362cf3a3d8163
nicole-gathany <nicole.gathany@gmail.com>
2021-06-14 23:27:01 -0400

    finally got my solution to work

--- linked_lists/find_nth/my_solution/index.js
+++ linked_lists/find_nth/my_solution/index.js
@@ -6,22 +6,23 @@ function Node(data) {
   //this worked for me but it's telling me invalid indexes should throw an error
   //for getNth(list, 100) where list is 1 -> 2 -> 3 -> null, it thows an error
   //not sure what i'm doing wrong but i'm going to look at the solution
-  
+  // this solution worked
+
   function getNth(node, index) {
     // Your code goes here.
-    //console.log(node, index);
-    
+    //console.log(node)
     let count = 0;
     let head = node;
-    //console.log(node.data)
     if(index===0) return node;
     while(head !== null){
-      head = head.next
-      //console.log(count)
-      count++
-      //console.log(head)
-      if(count === index) return head;
+      head = head.next;
+       count++
+      console.log("count", count);
+      if(count === index && head) return head;
+      console.log(head)
     }
-  
+    
+    if(!head) throw new Error('Invalid node at ' + index);
+    
   }
   
