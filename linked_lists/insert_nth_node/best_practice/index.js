5af97f0ecc015184ef45f6784a0f8b4b9de49e53
nicole-gathany <nicole.gathany@gmail.com>
2021-06-15 23:45:47 -0400

    added an alternative best practice solution and notes

--- linked_lists/insert_nth_node/best_practice/index.js
+++ linked_lists/insert_nth_node/best_practice/index.js
@@ -10,9 +10,25 @@ function Node(data) {
   function insertNth(head, index, data) {
     if (index == 0) {
       return push(head, data);
-    }
+    } 
+    //push was in one of the other problems, but what happens if you  didn't know about that problem??
   
     head.next = insertNth(head.next, index - 1, data);
   
     return head;
   }
+
+  //alternative solution
+
+//   function Node(data, nxt) {
+//     this.data = data;
+//     this.next = nxt; //this makes sense but how would they have known to change this part of the equation
+//   }
+//   function insertNth(head, index, data) {
+//     if(index == 0) return new Node(data, head);
+//     if(head && index > 0) {
+//       head.next = insertNth(head.next, index - 1, data);
+//       return head;
+//     }
+//     throw "Error";
+//   }
