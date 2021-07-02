5076f70fce02679f59e8e2990fbb9cf7b7077a67
nicole-gathany <nicole.gathany@gmail.com>
2021-07-02 11:26:11 -0400

    7kyu strings problem

---
+++ strings/dna/best_practice/index.js
@@ -0,0 +1,7 @@
+var pairs = {'A':'T','T':'A','C':'G','G':'C'};
+
+function DNAStrand(dna){
+  return dna.split('').map(function(v){ return pairs[v] }).join('');
+}
+
+//i find this one really interesting because i'm interested in objects as data structures
