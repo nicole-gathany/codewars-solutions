e4c1a844117746589544618a605516efff3d601b
nicole-gathany <nicole.gathany@gmail.com>
2021-07-14 10:18:54 -0400

    best practice and then some notes

--- strings/break_camelCase/best_pratice/index.js
+++ strings/break_camelCase/best_pratice/index.js
@@ -1,9 +1,12 @@
 function solution(string) {
-    string = string.split('').map(function (el) {
-      if (el === el.toUpperCase()) {
-        el = ' ' + el
+    string = string.split('').map(function (el) { //splits the whole string into an array
+        //delineated by no space so "tina" is ["t", "i", "n", "a"]
+      if (el === el.toUpperCase()) { //if the letter is uppercase 
+        el = ' ' + el //then add a space before it
       }
-      return el
+      return el //otherwise return the letter
     })
-    return string.join('')
+    return string.join('') //the join the array
   }
+
+  //i like this solution to the problem because it's a lot easier to read and understand
