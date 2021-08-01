bb7881b04351a6e4bc3cb3bfd0ec5c09f160b98e
nicole-gathany <nicole.gathany@gmail.com>
2021-08-01 01:06:42 -0400

    finished object-oriented problem

--- object_oriented_programming/create_router/my_solution/index.js
+++ object_oriented_programming/create_router/my_solution/index.js
@@ -1,12 +1,49 @@
 var Router = function()
 {
+  //router.bind and router.runRequest more than once in a new Router
  this.bind = function(route, method, fn){
-    !this.route? this.route = route: this.route? this.route = [this.route, route]
-   this.method = method;
-   this.fn = fn();
-   //console.log(Object.values(this.obj))
+   //url, http method (both strings), 
+   //function
+   if(!this.route){
+     this.route = [route];
+   }
+   else if(this.route.includes(route) && this.method.includes(method)){
+     this.fn = [fn()]
+   }
+   else if(this.route && this.method){
+     if(!this.route.includes(route) || !this.method.includes(method)){
+       this.route.push(route);
+       this.method.push(method);
+       this.fn.push(fn())
+     }
+   }
+   
+   if(!this.method){
+     this.method = [method];
+   }
+  
+   if(!this.fn){
+     this.fn = [fn()]
+   }
+   
+   console.log(this.route.includes(route), this.method.includes(method))
+     
+    
+   
  }
   this.runRequest = function(routeCall, methodCall){
-    return this.fn
+    if(!this.route){
+      return 'Error 404: Not Found'
+    }
+    
+   if(this.route.includes(routeCall) && this.method.includes(methodCall)){ 
+    for(let i=0; i<this.method.length; i++){
+      if(this.method[i]===methodCall && this.route[i]===routeCall){
+        return this.fn[i]
+      }
+    }
+  }
+    
+    
   }
 }
